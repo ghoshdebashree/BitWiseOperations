3e5e27b668a70913a2b3f9d57884d1b91598d092
ghoshdebashree <shree19ghosh@gmail.com>
2024-01-30 16:55:42 +0530

    Initial commit

---
+++ src/SwapWithoutTemp.java
@@ -0,0 +1,10 @@
+public class SwapWithoutTemp {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        a = a ^ b; //USING XOR 
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping: a = " +a+ " and b = " +b);
+    }
+}
